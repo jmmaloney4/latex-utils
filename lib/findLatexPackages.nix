893898c4ed988846ad7305216cc0249842f002dd
Jack Maloney <jack@maloney.ai>
2023-03-13 20:16:47 -0500

    updates

--- lib/findLatexPackages.nix
+++ lib/findLatexPackages.nix
@@ -7,11 +7,11 @@
   lineToPackageName = (line:
     let
       exact = builtins.match ''\\usepackage\{([A-Za-z0-9_]*).*'' line;
-      squarebrackets = builtins.match ''\\usepackage\\[.*\\]\{(.*)\}.*'' line;
+      # squarebrackets = builtins.match ''\\usepackage\\[.*\\]\{(.*)\}.*'' line;
       comment = builtins.match ''^\\usepackage.*% CTAN: ([A-Za-z0-9]*)$'' line;
     in
       if (comment != null) then (elemAt comment 0) else
-      if (squarebrackets != null) then (elemAt squarebrackets 0) else
+      # if (squarebrackets != null) then (elemAt squarebrackets 0) else
       if (exact != null) then (elemAt exact 0) else
       null
   );
