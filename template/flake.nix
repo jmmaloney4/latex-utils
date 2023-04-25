88ff255f8c560a19256e354f53515caf40c28ae4
Jack Maloney <jack@maloney.ai>
2023-04-25 15:39:44 -0500

    update template

--- template/flake.nix
+++ template/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-22.11;
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
     flake-utils.url = github:numtide/flake-utils;
     latex-utils = {
       url = "github:jmmaloney4/latex-utils";
