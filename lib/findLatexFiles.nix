ba05e1396d569ea4308dbbd9f376655ac8c683b7
Jack Maloney <jack@maloney.ai>
2023-07-04 11:49:47 -0500

    work on recursive latex files

--- lib/findLatexFiles.nix
+++ lib/findLatexFiles.nix
@@ -10,10 +10,11 @@ let
   processDirectory = rootPath: extensions: let
     readDir = builtins.readDir rootPath;
 
-    # paths = traceVal (mapAttrs (name: type: rootPath + "/${name}") readDir);
+    # paths = traceVal (mapAttrs (  name: type: rootPath + "/${name}") readDir);
     directories = attrNames (filterAttrs (name: type: type == "directory") readDir);
     files = attrNames (filterAttrs (name: type: type == "regular") readDir);
     filesWithExtensions = filter (name: lists.any (ext: strings.hasSuffix ext name) extensions) files;
+    fullPaths = map (name: rootPath + "/${name}") filesWithExtensions;
     recuriveFiles = builtins.concatLists (map (name: processDirectory (rootPath + "/${name}") extensions) directories);
   in
     filesWithExtensions ++ recuriveFiles;
